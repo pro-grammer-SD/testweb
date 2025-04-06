97bc38db6fed46adc490512846a528fd3741d6d2
pro-grammer-SD <geniussantu1983@gmail.com>
2025-04-06 17:59:26 +0530

    up creator

--- init.bat
+++ init.bat
@@ -1,10 +1,17 @@
 @echo off
 set /p PROJECTNAME=Enter your Django project name: 
+set /p APPNAME=Enter your Django app name: 
+
 python -m venv venv
 call venv\Scripts\activate
+
 python -m pip install --upgrade pip
 pip install django gunicorn
+
 django-admin startproject %PROJECTNAME%
 cd %PROJECTNAME%
-echo Done! Your Django project '%PROJECTNAME%' is ready and inside its own venv.
+python manage.py startapp %APPNAME%
+
+echo.
+echo Done! Your Django project '%PROJECTNAME%' with app '%APPNAME%' is ready and inside its own venv.
 pause
